bb5bc4723313e6b51e0dffc6ad7c54786d017561
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-03-15 17:53:04 +0900

    Filter AmplifyComponents

--- Assets/UrMotion/Scripts/Motion/FluentSyntax/FilterSyntax.cs
+++ Assets/UrMotion/Scripts/Motion/FluentSyntax/FilterSyntax.cs
@@ -52,6 +52,17 @@ namespace UrMotion
 			return self;
 		}
 
+		public static MotionBehaviour<V> AmplifyComponents<V, T>(this MotionBehaviour<V> self, T value)
+		{
+			Syntax.Resolve<V>(self,
+				(e) => e.Wrap((v) => Filter.AmplifyComponents(v, Syntax.AsEnumerator<float,   T>(value))),
+				(e) => e.Wrap((v) => Filter.AmplifyComponents(v, Syntax.AsEnumerator<Vector2, T>(value))),
+				(e) => e.Wrap((v) => Filter.AmplifyComponents(v, Syntax.AsEnumerator<Vector3, T>(value))),
+				(e) => e.Wrap((v) => Filter.AmplifyComponents(v, Syntax.AsEnumerator<Vector4, T>(value)))
+			);
+			return self;
+		}
+
 		public static MotionBehaviour<V> Discrete<V, T>(this MotionBehaviour<V> self, T interval)
 		{
 			Syntax.Resolve<V>(self,
@@ -71,6 +82,26 @@ namespace UrMotion
 		public static MotionBehaviour<V> Amplify<V>(this MotionBehaviour<V> self, Func<float> value)
 		{
 			return Amplify<V, Func<float>>(self, value);
+		}
+
+		public static MotionBehaviour<V> AmplifyComponents<V>(this MotionBehaviour<V> self, Func<float> value)
+		{
+			return AmplifyComponents<V, Func<float>>(self, value);
+		}
+
+		public static MotionBehaviour<V> AmplifyComponents<V>(this MotionBehaviour<V> self, Func<Vector2> value)
+		{
+			return AmplifyComponents<V, Func<Vector2>>(self, value);
+		}
+
+		public static MotionBehaviour<V> AmplifyComponents<V>(this MotionBehaviour<V> self, Func<Vector3> value)
+		{
+			return AmplifyComponents<V, Func<Vector3>>(self, value);
+		}
+
+		public static MotionBehaviour<V> AmplifyComponents<V>(this MotionBehaviour<V> self, Func<Vector4> value)
+		{
+			return AmplifyComponents<V, Func<Vector4>>(self, value);
 		}
 
 		public static MotionBehaviour<V> Discrete<V>(this MotionBehaviour<V> self, Func<float> interval)
@@ -119,6 +150,18 @@ namespace UrMotion
 			return res;
 		}
 
+		public static IEnumerator<V> AmplifyComponents<V, T>(this IEnumerator<V> source, T value)
+		{
+			var res = default(IEnumerator<V>);
+			Syntax.Resolve<V>(
+				() => res = Filter.AmplifyComponents(source as IEnumerator<float  >, Syntax.AsEnumerator<float,   T>(value)) as IEnumerator<V>,
+				() => res = Filter.AmplifyComponents(source as IEnumerator<Vector2>, Syntax.AsEnumerator<Vector2, T>(value)) as IEnumerator<V>,
+				() => res = Filter.AmplifyComponents(source as IEnumerator<Vector3>, Syntax.AsEnumerator<Vector3, T>(value)) as IEnumerator<V>,
+				() => res = Filter.AmplifyComponents(source as IEnumerator<Vector4>, Syntax.AsEnumerator<Vector4, T>(value)) as IEnumerator<V>
+			);
+			return res;
+		}
+
 		public static IEnumerator<V> Discrete<V, T>(this IEnumerator<V> source, T interval)
 		{
 			var res = default(IEnumerator<V>);
@@ -149,6 +192,26 @@ namespace UrMotion
 		public static IEnumerator<V> Amplify<V>(this IEnumerator<V> source, Func<float> value)
 		{
 			return Amplify<V, Func<float>>(source, value);
+		}
+
+		public static IEnumerator<V> AmplifyComponents<V>(this IEnumerator<V> source, Func<float> value)
+		{
+			return AmplifyComponents<V, Func<float>>(source, value);
+		}
+
+		public static IEnumerator<V> AmplifyComponents<V>(this IEnumerator<V> source, Func<Vector2> value)
+		{
+			return AmplifyComponents<V, Func<Vector2>>(source, value);
+		}
+
+		public static IEnumerator<V> AmplifyComponents<V>(this IEnumerator<V> source, Func<Vector3> value)
+		{
+			return AmplifyComponents<V, Func<Vector3>>(source, value);
+		}
+
+		public static IEnumerator<V> AmplifyComponents<V>(this IEnumerator<V> source, Func<Vector4> value)
+		{
+			return AmplifyComponents<V, Func<Vector4>>(source, value);
 		}
 
 		public static IEnumerator<V> Discrete<V>(this IEnumerator<V> source, Func<float> interval)
