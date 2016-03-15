4e8873886ac5b4492ce78633a62d8ef179047cf5
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-03-15 11:28:21 +0900

    LifeCycle Threshold

---
+++ Assets/UrMotion/Scripts/Motion/Comparator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace UrMotion
+{
+	public static class Comparator
+	{
+		public static readonly Func<float, float, bool> LessThan = (a, b) => a < b;
+		public static readonly Func<float, float, bool> GreaterThan = (a, b) => a > b;
+		public static readonly Func<float, float, bool> LessThanOrEqualTo = (a, b) => a <= b;
+		public static readonly Func<float, float, bool> GreaterThanOrEqualTo = (a, b) => a >= b;
+		public static readonly Func<float, float, bool> EqualTo = (a, b) => a == b;
+		public static readonly Func<float, float, bool> Approximately = (a, b) => Mathf.Approximately(a, b);
+		public static Func<float, float, bool> Near(float threshold)
+		{
+			return (a, b) => Mathf.Abs(b - a) <= threshold;
+		}
+	}
+}
