a7bc9a0230897b452651996077c4c193055a5cde
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-03-15 17:53:28 +0900

    Noise Perlin

---
+++ Assets/UrMotion/Scripts/Motion/FluentSyntax/NoiseSyntax.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace UrMotion
+{
+	using Syntax = UrMotion.FluentSyntax;
+
+	public static class NoiseSyntax
+	{
+		public static MotionBehaviour<V> Perlin<V>(this MotionBehaviour<V> self, V speed)
+		{
+			Syntax.Resolve<V>(self,
+				(e) => e.Add(Noise.Perlin((float  )(object)speed, self.FrameRate)),
+				(e) => e.Add(Noise.Perlin((Vector2)(object)speed, self.FrameRate)),
+				(e) => e.Add(Noise.Perlin((Vector3)(object)speed, self.FrameRate)),
+				(e) => e.Add(Noise.Perlin((Vector4)(object)speed, self.FrameRate))
+			);
+			return self;
+		}
+		public static MotionBehaviour<V> Perlin<V>(this MotionBehaviour<V> self, V offset, V speed)
+		{
+			Syntax.Resolve<V>(self,
+				(e) => e.Add(Noise.PerlinWith((float  )(object)offset, (float  )(object)speed, self.FrameRate)),
+				(e) => e.Add(Noise.PerlinWith((Vector2)(object)offset, (Vector2)(object)speed, self.FrameRate)),
+				(e) => e.Add(Noise.PerlinWith((Vector3)(object)offset, (Vector3)(object)speed, self.FrameRate)),
+				(e) => e.Add(Noise.PerlinWith((Vector4)(object)offset, (Vector4)(object)speed, self.FrameRate))
+			);
+			return self;
+		}
+	}
+}
---
+++ Assets/UrMotion/Scripts/Motion/Noise.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace UrMotion
+{
+	using P = Klak.Math.Perlin;
+
+	public static class Noise
+	{
+		public static IEnumerator<float> Perlin(float speed, float fps = 0f)
+		{
+			return PerlinWith(Random.Range(-10000f, 0f), speed, fps);
+		}
+
+		public static IEnumerator<Vector2> Perlin(Vector2 speed, float fps = 0f)
+		{
+			return PerlinWith(new Vector2(Random.Range(-10000f, 0f), Random.Range(-10000f, 0f)), speed, fps);
+		}
+
+		public static IEnumerator<Vector3> Perlin(Vector3 speed, float fps = 0f)
+		{
+			return PerlinWith(new Vector3(Random.Range(-10000f, 0f), Random.Range(-10000f, 0f), Random.Range(-10000f, 0f)), speed, fps);
+		}
+
+		public static IEnumerator<Vector4> Perlin(Vector4 speed, float fps = 0f)
+		{
+			return PerlinWith(new Vector4(Random.Range(-10000f, 0f), Random.Range(-10000f, 0f), Random.Range(-10000f, 0f), Random.Range(-10000f, 0f)), speed, fps);
+		}
+
+		public static IEnumerator<float> PerlinWith(float offset, float speed, float fps = 0f)
+		{
+			Source.ValidateFrameRate(ref fps);
+			var f = 0f;
+			for (;;) {
+				var t = offset + (f / fps) * speed;
+				yield return P.Noise(t);
+				f += 1.0f;
+			}
+		}
+
+		public static IEnumerator<Vector2> PerlinWith(Vector2 offset, Vector2 speed, float fps = 0f)
+		{
+			Source.ValidateFrameRate(ref fps);
+			var f = 0f;
+			for (;;) {
+				var t = offset + (f / fps) * speed;
+				yield return new Vector2(P.Noise(t.x), P.Noise(t.y));
+				f += 1.0f;
+			}
+		}
+
+		public static IEnumerator<Vector3> PerlinWith(Vector3 offset, Vector3 speed, float fps = 0f)
+		{
+			Source.ValidateFrameRate(ref fps);
+			var f = 0f;
+			for (;;) {
+				var t = offset + (f / fps) * speed;
+				yield return new Vector3(P.Noise(t.x), P.Noise(t.y), P.Noise(t.z));
+				f += 1.0f;
+			}
+		}
+
+		public static IEnumerator<Vector4> PerlinWith(Vector4 offset, Vector4 speed, float fps = 0f)
+		{
+			Source.ValidateFrameRate(ref fps);
+			var f = 0f;
+			for (;;) {
+				var t = offset + (f / fps) * speed;
+				yield return new Vector4(P.Noise(t.x), P.Noise(t.y), P.Noise(t.z), P.Noise(t.w));
+				f += 1.0f;
+			}
+		}
+	}
+}
