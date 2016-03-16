8471ad194e4c14c81c5f32e04e3b5b00b4743da9
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-03-16 11:23:55 +0900

    Speed of circulic move is now controllable

--- Assets/UrMotion/Scripts/Motion/Movement.cs
+++ Assets/UrMotion/Scripts/Motion/Movement.cs
@@ -6,67 +6,67 @@ namespace UrMotion
 {
 	public static class Movement
 	{
-		public static IEnumerator<float> Sin(IEnumerator<float> radius, float freq, float fps)
+		public static IEnumerator<float> Sin(IEnumerator<float> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Sin(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector2> Sin(IEnumerator<Vector2> radius, float freq, float fps)
+		public static IEnumerator<Vector2> Sin(IEnumerator<Vector2> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Sin(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector3> Sin(IEnumerator<Vector3> radius, float freq, float fps)
+		public static IEnumerator<Vector3> Sin(IEnumerator<Vector3> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Sin(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector4> Sin(IEnumerator<Vector4> radius, float freq, float fps)
+		public static IEnumerator<Vector4> Sin(IEnumerator<Vector4> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Sin(radius, freq, fps));
 		}
 
-		public static IEnumerator<float> Cos(IEnumerator<float> radius, float freq, float fps)
+		public static IEnumerator<float> Cos(IEnumerator<float> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Cos(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector2> Cos(IEnumerator<Vector2> radius, float freq, float fps)
+		public static IEnumerator<Vector2> Cos(IEnumerator<Vector2> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Cos(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector3> Cos(IEnumerator<Vector3> radius, float freq, float fps)
+		public static IEnumerator<Vector3> Cos(IEnumerator<Vector3> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Cos(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector4> Cos(IEnumerator<Vector4> radius, float freq, float fps)
+		public static IEnumerator<Vector4> Cos(IEnumerator<Vector4> radius, IEnumerator<float> freq, float fps)
 		{
 			return Move(Source.Cos(radius, freq, fps));
 		}
 
-		public static IEnumerator<Vector2> Circular(IEnumerator<float> radius, float speed, float fps)
+		public static IEnumerator<Vector2> Circular(IEnumerator<float> radius, IEnumerator<float> speed, float fps)
 		{
 			return Move(Source.Circular(radius, speed, fps));
 		}
 
-		public static IEnumerator<Vector2> Lissajous(IEnumerator<float> A, IEnumerator<float> B, float a, float b, float delta, float fps)
+		public static IEnumerator<Vector2> Lissajous(IEnumerator<float> A, IEnumerator<float> B, IEnumerator<float> a, IEnumerator<float> b, float delta, float fps)
 		{
 			return Move(Source.Lissajous(A, B, a, b, delta, fps));
 		}
 
-		public static IEnumerator<Vector2> Cycloid(IEnumerator<float> A, IEnumerator<float> B, float rm, float speed, float fps)
+		public static IEnumerator<Vector2> Cycloid(IEnumerator<float> A, IEnumerator<float> B, float rm, IEnumerator<float> speed, float fps)
 		{
 			return Move(Source.Cycloid(A, B, rm, speed, fps));
 		}
 
-		public static IEnumerator<Vector2> Epicycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, float speed, float fps)
+		public static IEnumerator<Vector2> Epicycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, IEnumerator<float> speed, float fps)
 		{
 			return Move(Source.Epicycloid(A, B, rc, rm, speed, fps));
 		}
 
-		public static IEnumerator<Vector2> Hypocycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, float speed, float fps)
+		public static IEnumerator<Vector2> Hypocycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, IEnumerator<float> speed, float fps)
 		{
 			return Move(Source.Hypocycloid(A, B, rc, rm, speed, fps));
 		}
--- Assets/UrMotion/Scripts/Motion/Source.cs
+++ Assets/UrMotion/Scripts/Motion/Source.cs
@@ -36,146 +36,145 @@ namespace UrMotion
 			}
 		}
 
-		public static IEnumerator<float> Sin(IEnumerator<float> radius, float freq, float fps = 0f)
+		public static IEnumerator<float> Sin(IEnumerator<float> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Sin((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Sin(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Sin(IEnumerator<Vector2> radius, float freq, float fps = 0f)
+		public static IEnumerator<Vector2> Sin(IEnumerator<Vector2> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Sin((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Sin(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector3> Sin(IEnumerator<Vector3> radius, float freq, float fps = 0f)
+		public static IEnumerator<Vector3> Sin(IEnumerator<Vector3> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Sin((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Sin(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector4> Sin(IEnumerator<Vector4> radius, float freq, float fps = 0f)
+		public static IEnumerator<Vector4> Sin(IEnumerator<Vector4> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Sin((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Sin(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<float> Cos(IEnumerator<float> radius, float freq, float fps = 0f)
+		public static IEnumerator<float> Cos(IEnumerator<float> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Cos((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Cos(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Cos(IEnumerator<Vector2> radius, float freq, float fps = 0f)
+		public static IEnumerator<Vector2> Cos(IEnumerator<Vector2> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Cos((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Cos(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector3> Cos(IEnumerator<Vector3> radius, float freq, float fps = 0f)
+		public static IEnumerator<Vector3> Cos(IEnumerator<Vector3> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Cos((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Cos(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector4> Cos(IEnumerator<Vector4> radius, float freq, float fps = 0f)
+		public static IEnumerator<Vector4> Cos(IEnumerator<Vector4> radius, IEnumerator<float> freq, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				yield return Mathf.Cos((f / fps) * Mathf.PI * 2f * freq) * radius.Current;
-				f += 1.0f;
+			var angle = 0f;
+			while (radius.MoveNext() && freq.MoveNext()) {
+				yield return Mathf.Cos(angle) * radius.Current;
+				angle += (1.0f / fps) * Mathf.PI * 2f * freq.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Circular(IEnumerator<float> radius, float speed, float fps = 0f)
+		public static IEnumerator<Vector2> Circular(IEnumerator<float> radius, IEnumerator<float> speed, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (radius.MoveNext()) {
-				var x = Mathf.Cos((f / fps) * Mathf.PI * 2f * speed) * radius.Current;
-				var y = Mathf.Sin((f / fps) * Mathf.PI * 2f * speed) * radius.Current;
+			var angle = 0f;
+			while (radius.MoveNext() && speed.MoveNext()) {
+				var x = Mathf.Cos(angle) * radius.Current;
+				var y = Mathf.Sin(angle) * radius.Current;
 				yield return new Vector2(x, y);
-				f += 1.0f;
+				angle += (1.0f / fps) * Mathf.PI * 2f * speed.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Lissajous(IEnumerator<float> A, IEnumerator<float> B, float a, float b, float delta, float fps = 0f)
+		public static IEnumerator<Vector2> Lissajous(IEnumerator<float> A, IEnumerator<float> B, IEnumerator<float> a, IEnumerator<float> b, float delta, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (A.MoveNext() && B.MoveNext()) {
-				var x = A.Current * Mathf.Cos((f / fps) * Mathf.PI * 2f * a);
-				var y = B.Current * Mathf.Sin((f / fps) * Mathf.PI * 2f * b + delta);
+			var angleA = 0f;
+			var angleB = 0f;
+			while (A.MoveNext() && B.MoveNext() && a.MoveNext() && b.MoveNext()) {
+				var x = A.Current * Mathf.Cos(angleA);
+				var y = B.Current * Mathf.Sin(angleB + delta);
 				yield return new Vector2(x, y);
-				f += 1.0f;
+				angleA += (1.0f / fps) * Mathf.PI * 2f * a.Current;
+				angleB += (1.0f / fps) * Mathf.PI * 2f * b.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Cycloid(IEnumerator<float> A, IEnumerator<float> B, float rm, float speed, float fps = 0f)
+		public static IEnumerator<Vector2> Cycloid(IEnumerator<float> A, IEnumerator<float> B, float rm, IEnumerator<float> speed, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (A.MoveNext() && B.MoveNext()) {
-				var t = (f / fps) * Mathf.PI * 2f * speed;
-				var x = A.Current * (rm * (t - Mathf.Sin(t)));
-				var y = B.Current * (rm * (1f - Mathf.Cos(t)));
+			var angle = 0f;
+			while (A.MoveNext() && B.MoveNext() && speed.MoveNext()) {
+				var x = A.Current * (rm * (angle - Mathf.Sin(angle)));
+				var y = B.Current * (rm * (1f - Mathf.Cos(angle)));
 				yield return new Vector2(x, y);
-				f += 1.0f;
+				angle += (1.0f / fps) * Mathf.PI * 2f * speed.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Epicycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, float speed, float fps = 0f)
+		public static IEnumerator<Vector2> Epicycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, IEnumerator<float> speed, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (A.MoveNext() && B.MoveNext()) {
-				var t = (f / fps) * Mathf.PI * 2f * speed;
-				var x = A.Current * ((rc + rm) * Mathf.Cos(t) - rm * Mathf.Cos((rc + rm) / rm * t));
-				var y = B.Current * ((rc + rm) * Mathf.Sin(t) - rm * Mathf.Sin((rc + rm) / rm * t));
+			var angle = 0f;
+			while (A.MoveNext() && B.MoveNext() && speed.MoveNext()) {
+				var x = A.Current * ((rc + rm) * Mathf.Cos(angle) - rm * Mathf.Cos((rc + rm) / rm * angle));
+				var y = B.Current * ((rc + rm) * Mathf.Sin(angle) - rm * Mathf.Sin((rc + rm) / rm * angle));
 				yield return new Vector2(x, y);
-				f += 1.0f;
+				angle += (1.0f / fps) * Mathf.PI * 2f * speed.Current;
 			}
 		}
 
-		public static IEnumerator<Vector2> Hypocycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, float speed, float fps = 0f)
+		public static IEnumerator<Vector2> Hypocycloid(IEnumerator<float> A, IEnumerator<float> B, float rc, float rm, IEnumerator<float> speed, float fps = 0f)
 		{
 			ValidateFrameRate(ref fps);
-			var f = 0f;
-			while (A.MoveNext() && B.MoveNext()) {
-				var t = (f / fps) * Mathf.PI * 2f * speed;
-				var x = A.Current * ((rc - rm) * Mathf.Cos(t) + rm * Mathf.Cos((rc - rm) / rm * t));
-				var y = B.Current * ((rc - rm) * Mathf.Sin(t) - rm * Mathf.Sin((rc - rm) / rm * t));
+			var angle = 0f;
+			while (A.MoveNext() && B.MoveNext() && speed.MoveNext()) {
+				var x = A.Current * ((rc - rm) * Mathf.Cos(angle) + rm * Mathf.Cos((rc - rm) / rm * angle));
+				var y = B.Current * ((rc - rm) * Mathf.Sin(angle) - rm * Mathf.Sin((rc - rm) / rm * angle));
 				yield return new Vector2(x, y);
-				f += 1.0f;
+				angle += (1.0f / fps) * Mathf.PI * 2f * speed.Current;
 			}
 		}
 	}
